3a470ca8572c9aab0430af7c0a7ed2c8ea12a19b
Mafinar K <mafinar@gmail.com>
2018-12-15 09:50:53 -0500

    Initialize repository

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:tmdb_dart/tmdb_dart.dart' as tmdb_dart;
+
+main(List<String> arguments) =>
+    print('Hello world: ${tmdb_dart.calculate()}!');
+
---
+++ test/tmdb_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tmdb_dart/tmdb_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
